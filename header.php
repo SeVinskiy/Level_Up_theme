8244e5010cc0daf80ac4c548a7588717340b8293
SeVinskiy <Sevik@ua.fm>
2017-01-06 16:02:21 +0200

    add menu for curses

--- header.php
+++ header.php
@@ -4,21 +4,21 @@
 	<meta charset="UTF-8" name="viewport" name="viewport" content="width=device-width, initial-scale=1">
 	<title>Level_UP</title>
 	<!-- <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"> -->
-	
-	<?php wp_head(); ?> 
+
+	<?php wp_head(); ?>
 </head>
 <body>
 
 	<header>
-		
+
 		<?php wp_nav_menu(array('theme_location'=>'main_menu', 'container_class' => 'containerHeaderSiteMenu',) ); ?>
 
 		<div class="headerNameCenter"><p>Учебный центр</p><p>подготовки IT-специалистов</p></div>
 
 		<div class="containerHeaderLogo">
 			<div class="headerContactLeft">
-				<p>Центральный офис</p>
-				<p>г.Днепр, ул.Троицкая,21г</p>
+				<p>Центральный офис, г.Днепр</p>
+				<p>ул.Троицкая,21г</p>
 				<p>ТСК "Новый металлург", 3 эт.</p>
 				<p>+38(056)376 90 66</p>
 				<p>+38(099)731 83 85, (096) 084 25 13</p>
@@ -26,8 +26,8 @@
 			</div>
 			<img src="<?php bloginfo('template_url'); ?>/img/logo.jpg">
 			<div class="headerContactRight">
-				<p>Филиал</p>
-				<p>г.Днепр, ул.Запорожское шоссе,53Б</p>
+				<p>Филиал, г.Днепр</p>
+				<p>ул.Запорожское шоссе,53Б</p>
 				<p>2-ой этаж</p>
 				<p>+38(095)708 78 76</p>
 				<p>+38(068)211 60 50</p>
@@ -38,12 +38,76 @@
  		<div class="containerHeaderKursMenu container">
 			<ul>
 				<li><a href="#">Открыт набор</a></li>
-				<li>Курсы начальные <i class="fa fa-caret-down" aria-hidden="true"></i></li>
-				<li>Основное <i class="fa fa-caret-down" aria-hidden="true"></i></li>
+				<li>Курсы начальные <span><i class="fa fa-caret-down" aria-hidden="true"></i></span>
+					<ul>
+							<li class="insertedLiKurs">Пользователь ПК <i class="fa fa-caret-down" aria-hidden="true"></i>
+									<ul>
+										<li><a href="#"><i class="fa fa-windows" aria-hidden="true"></i> Использование компьютера с ОС Windows</a></li>
+										<li><a href="#"><i class="fa fa-apple" aria-hidden="true"></i> Использование компьютера Apple Mac</a></li>
+										<li><a href="#"><i class="fa fa-indent" aria-hidden="true"></i> Курсы Microsoft Office</a></li>
+									</ul>
+							</li>
+							<li class="insertedLiKurs">Базовые курсы <i class="fa fa-caret-down" aria-hidden="true"></i>
+									<ul>
+										<li><a href=""><i class="fa fa-star" aria-hidden="true"></i> Интернет-маркетинг</a></li>
+										<li><a href=""><i class="fa fa-share-square" aria-hidden="true"></i> Программирование на языке Python</a></li>
+										<li><a href=""><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Курс видеомонтажа — Adobe Premiere Pro</a></li>
+										<li><a href=""><i class="fa fa-cogs" aria-hidden="true"></i> Курс “Основы программирования”</a></li>
+										<li><a href=""><i class="fa fa-star" aria-hidden="true"></i> Базовые основы ООП (основы программирования и С++)</a></li>
+										<li><a href=""><i class="fa fa-hdd-o" aria-hidden="true"></i> Основы программирования на С#</a></li>
+										<li><a href=""><i class="fa fa-html5" aria-hidden="true"></i> HTML5, CSS3, и создание сайтов на CMS</a></li>
+										<li><a href=""><i class="fa fa-tasks" aria-hidden="true"></i> Управление базами данных SQL</a></li>
+									</ul>
+							</li>
+							<li class="insertedLiKurs">Дизайн <i class="fa fa-caret-down" aria-hidden="true"></i>
+								<ul>
+									<li><a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i> Курс Web/UI дизайн</a></li>
+									<li><a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i> Основы дизайна</a></li>
+									<li><a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> 3D моделирование. Базовый курс</a></li>
+									<li><a href="#"><i class="fa fa-eye" aria-hidden="true"></i> Графический редактор Adobe Photoshop</a></li>
+									<li><a href="#"><i class="fa fa-mobile" aria-hidden="true"></i> Product Design (UI&amp;UX Design)</a></li>
+								</ul>
+							</li>
+					</ul>
+				</li>
+				<li>Основное <span><i class="fa fa-caret-down" aria-hidden="true"></i></span>
+					<ul>
+							<li class="insertedLiKurs">Разработка программного обеспечения <i class="fa fa-caret-down" aria-hidden="true"></i>
+								<ul>
+									<li><a href="#"><i class="fa fa-pencil" aria-hidden="true"></i> Курс С# и объектно-ориентированное программирование</a></li>
+									<li><a href="#"><i class="fa fa-wrench" aria-hidden="true"></i> Тестирование программного обеспечения</a></li>
+									<li><a href="#"><i class="fa fa-windows" aria-hidden="true"></i> Курс разработки приложений на платформе Microsoft.NET</a></li>
+									<li><a href="#"><i class="fa fa-pencil" aria-hidden="true"></i> Программирование на 1С</a></li>
+									<li><a href="#"><i class="fa fa-github" aria-hidden="true"></i> Программирование на Java</a></li>
+									<li><a href="#"><i class="fa fa-android" aria-hidden="true"></i> Java программирование под Android</a></li>
+									<li><a href="#"><i class="fa fa-apple" aria-hidden="true"></i> Разработка приложений под iOS</a></li>
+								</ul>
+							</li>
+							<li class="insertedLiKurs">Разработка WEB-проектов <i class="fa fa-caret-down" aria-hidden="true"></i>
+								<ul>
+									<li><a href="#"><i class="fa fa-css3" aria-hidden="true"></i> Front-end разработка</a></li>
+									<li><a href="#"><i class="fa fa-html5" aria-hidden="true"></i> Веб-разработка с использованием PHP</a></li>
+									<li><a href="#"><i class="fa fa-rocket" aria-hidden="true"></i> JavaScript</a></li>
+									<li><a href="#"><i class="fa fa-html5" aria-hidden="true"></i> Разработка WEB-приложений на Ruby on Rails</a></li>
+									<li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Разработка под Magento</a></li>
+								</ul>
+							</li>
+							<li class="insertedLiKurs">IT-менеджмент <i class="fa fa-caret-down" aria-hidden="true"></i>
+								<ul>
+									<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i> Soft skills for IT</a></li>
+									<li><a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i> IT English Course</a></li>
+									<li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> Курсы Project Management</a></li>
+									<li><a href="#"><i class="fa fa-bullhorn" aria-hidden="true"></i> Курс ораторское мастерство</a></li>
+									<li><a href="#"><i class="fa fa-indent" aria-hidden="true"></i> Scrum Fundamentals</a></li>
+									<li><a href="#"><i class="fa fa-indent" aria-hidden="true"></i> Обучение IT-рекрутингу от ведущих специалистов Украины</a></li>
+								</ul>
+							</li>
+					</ul>
+				</li>
 				<li><a href="#">IT-инкубатор</a></li>
 				<li><a href="#">Карьера в IT</a></li>
 				<li><a href="#">Детская IT-школа</a></li>
-				<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a> <input class="findKurs" type="text" name="kurs" placeholder="Поиск курса..." autofocus=""></li>
+				<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a> <div class="inputContainer"><input class="findKurs" type="text" name="kurs" placeholder="Поиск курса..."><div></li>
 			</ul>
 		</div>
 	</header>
--- js/main.js
+++ js/main.js
@@ -1,49 +1,74 @@
 ;(function() {
 	"use strict";
 
-	//смещение главной менюшки, менюшки курсов и ее анимация
+	//действия при смене ширины экрана или загрузке страницы
 	function windowSize(){
-		if ($(window).width() < '769'){ 
+		if ($(window).width() < '769'){
+				//основное меню
+				if(!$('.containerHeaderSiteMenu ul li i').hasClass("fa-bars")) {
+	        $(".containerHeaderSiteMenu ul").append(`<li class="liMiniMenu MainMenuIsCloced"><i class="fa fa-bars" aria-hidden="true"></i> Меню сайта <i class="fa fa-hand-o-down" aria-hidden="true"></i></li>`);
+	      }
+
+				//меню курсов
 				$(".containerHeaderKursMenu").addClass("minimazeKursMenu");
-				$(".findKurs").css({"display": "inline"});  
+
+				//поиск курса
+				$("div.inputContainer").css({"display": "inline-block"});
+
+				//добавление иконки меню курсов
 				if(!$(".containerHeaderKursMenu i").hasClass("fa-th-list")) $(".containerHeaderKursMenu").append(`<i class="fa fa-th-list" aria-hidden="true"></i>`);
 
+				//разворачивание меню курсов
+				$("i.fa-th-list").click(function(event) {
+					event.stopImmediatePropagation();
+					$("div.minimazeKursMenu > ul").addClass("kursColumnDisplay");
+				});
 
-	      if(!$('.containerHeaderSiteMenu ul li i').hasClass("fa-bars")) {
-	        $(".containerHeaderSiteMenu ul").append(`<li class="liMiniMenu MainMenuIsCloced"><i class="fa fa-bars" aria-hidden="true"></i> Меню сайта <i class="fa fa-hand-o-down" aria-hidden="true"></i></li>`);
-	      }		
+				//сворачивание меню курсов
+				$(document.body).on("click", function(){
+					$("div.minimazeKursMenu > ul").removeClass("kursColumnDisplay");
+				});
+
+				kursMenu();
+
 	  } else {
 	  		$(".containerHeaderKursMenu").removeClass("minimazeKursMenu");
-	  		$(".findKurs").css({"display": "none"});
+	  		$("div.inputContainer").css({"display": "none"});
 	  		if($(".containerHeaderKursMenu i").hasClass("fa-th-list")) $(".containerHeaderKursMenu i.fa-th-list").remove();
 
+				//клики поиск курса
 	  		$("i.fa-search").click(function(event) {
 	  			event.stopImmediatePropagation();
-	  			$("input.findKurs").css({"display": "inline"});
+	  			$("div.inputContainer").css({"display": "inline-block"});
 	  		});
 
-	  		$(document.body).click(function(){
-	  			$("input.findKurs").css({"display": "none"});
-	  		});
- 		
+				//закрытие поиска курсов
+				$(document.body).on("click", function(){
+					$("div.inputContainer").css({"display": "none"});
+				});
+
 	  		if($('.containerHeaderSiteMenu ul li i').hasClass("fa-bars")) {
 	    		$(".containerHeaderSiteMenu ul li:last-child").remove();
-	    	}	
+	    	};
+
+				//закрытие меню курсов
+				$("div.containerHeaderKursMenu ul").removeClass("kursColumnDisplay");
 	    }
+
 	    //открытие главного меню
 	    $("li.MainMenuIsCloced").click(function(event){
 	    	event.stopImmediatePropagation();
 
 	    	if(!$(".containerHeaderSiteMenu ul li:last-child").hasClass("MainMenuIsOpen")) {
 		        $(".containerHeaderSiteMenu").css({"animation": "openMainMenuAction .7s linear forwards"});
-		        $("i.fa-hand-o-down").css({"animation": "hendPointerDown .7s linear forwards"}); 
+		        $("i.fa-hand-o-down").css({"animation": "hendPointerDown .7s linear forwards"});
 		        $(".containerHeaderSiteMenu ul li:last-child").removeClass("MainMenuIsCloced");
-		        $(".containerHeaderSiteMenu ul li:last-child").addClass("MainMenuIsOpen");   
+		        $(".containerHeaderSiteMenu ul li:last-child").addClass("MainMenuIsOpen");
 	    	};
 	    });
 
-	    //закрытие главного меню
 	    $(document.body).on("click", function(){
+				//закрытие главного меню
 	      if($(".containerHeaderSiteMenu ul li:last-child").hasClass("MainMenuIsOpen")) {
 			      $(".containerHeaderSiteMenu").css({"animation": "clocedMainMenuAction 1s linear forwards"});
 			      $("i.fa-hand-o-down").css({"animation": "hendPointerAp 1s linear forwards"});
@@ -52,30 +77,57 @@
 		    };
 		  });
 
-	    window.distanceTop = $('.containerHeaderKursMenu').offset().top;
+			kursMenu();
+
+	    window.distanceTop = $('.containerHeaderKursMenu').offset().top;			//var не видит условный оператор
 	};
 
 	$(window).on('load resize',windowSize);
 
-	
 
 	//появление элементов в секции 1
-	function opasitiElements() { 
+	function opasitiElements() {
 			$(".section1 h2").animate({ opacity: "1"}, 500);
-		setTimeout(function() { 
+		setTimeout(function() {
 		    $(".section1 h4").animate({ opacity: "1"}, 500);
 		}, 750);
-		setTimeout(function() { 
-		    $(".section1 img").animate({ opacity: "1"}, 500);
-		}, 1500);
+		setTimeout(function() {
+		    	$(".section1 img").animate({ opacity: "1"}, 500);
+				}, 1500);
 	};
 
 	$(window).on('load', opasitiElements);
 
 
-	
+	function kursMenu() {
+		//клики по основным/начальным курсам
+		$(`.containerHeaderKursMenu > ul > li:has(ul)`).click(function(event) {
+			event.stopImmediatePropagation();
+			if($("ul").hasClass("liChildKurs")) {		//проверка на наличие курсов
+				$("ul").removeClass("liChildKurs");
+			};
+			$('ul:first',this).addClass("liChildKurs");
+		});
 
-	//фиксация меню курсов
+		//клики по вложенным категориям курсов
+		$("li.insertedLiKurs").click(function(event) {
+			event.stopImmediatePropagation();
+			if($("ul").hasClass("secontInsertedUl")) {		//проверка на наличие открытых подменю курсов
+				$("ul").removeClass("secontInsertedUl");
+			};
+			$('ul:first',this).addClass("secontInsertedUl");
+		});
+
+		//закрытие менюшки курсов
+		$(document.body).on("click", function(){
+			if($("li ul").hasClass("liChildKurs")) {
+				$("ul").removeClass("liChildKurs");
+			};
+			$("ul").removeClass("secontInsertedUl");
+		});
+	}
+
+	//фиксация меню курсов при скроллинге
 	$(window).on("scroll", function() {
 			// var distanceTop = $('.containerHeaderKursMenu').offset().top;
 
@@ -83,16 +135,11 @@
     		$('.containerHeaderKursMenu').addClass('fixed');
     		$('.containerHeaderKursMenu').removeClass('container');
     		$(".section1").css({"margin-top": "109px"});
-    	}	else {			
+    	}	else {
     		$('.containerHeaderKursMenu').removeClass('fixed');
     		$('.containerHeaderKursMenu').addClass('container');
     		$(".section1").css({"margin-top": "20px"});
-    		}					
+    		}
   });
 
-	
 })();
-
-
-
-

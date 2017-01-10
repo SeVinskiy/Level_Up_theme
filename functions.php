82a2e846b004094c77c3c41f5b6a6097aee0f7c6
SeVinskiy <Sevik@ua.fm>
2017-01-10 12:21:28 +0200

    добавлены вендорные префиксы, удалены переменные из css, убрана ссылка на variables.css

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 function theme_name_scripts() {
-	
+
 	wp_enqueue_script( 'jquery.min', get_template_directory_uri() . '/js/jquery.min.js');
 	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js');
 	// wp_enqueue_style( 'style', get_stylesheet_uri());
 	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css');
-	wp_enqueue_style( 'variables', get_template_directory_uri() . '/css/variables.css');
+	// wp_enqueue_style( 'variables', get_template_directory_uri() . '/css/variables.css');
 	wp_enqueue_style( 'fonts', get_template_directory_uri() . '/css/fonts/fonts.css');
 	wp_enqueue_style( 'font-awesome.min', get_template_directory_uri() . '/css/fonts/font-awesome.min.css');
 }
